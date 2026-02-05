5e1161672c7ad501a0c5051af41721582854c460
Roberto R. <robertorodriguezbqz@gmail.com>
2026-02-05 17:21:20 -0300

    feat: inicio de gifs-app con angular 19, zoneless y flowbite

---
+++ src/app/app.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { OnInit } from '@angular/core';
+import { initFlowbite } from 'flowbite';
+
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css',
+})
+export class App  implements OnInit{
+  protected title = 'gifs-app';
+
+  ngOnInit(): void {
+    initFlowbite();
+  }
+}
