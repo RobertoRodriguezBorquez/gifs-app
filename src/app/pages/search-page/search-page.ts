12d4ee775cc8e515abd7dd9d719716bdeb874c28
Robertorodriguezbqz <robertorodriguezbqz@gmail.com>
2026-02-19 16:18:18 -0300

    buscador funcionando, falta agregar historial

--- src/app/pages/search-page/search-page.ts
+++ src/app/pages/search-page/search-page.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { GifService } from 'src/services/gif.service';
+import { Component, inject, signal } from '@angular/core';
+import { GifList } from 'src/app/gifs/components/gifs-side-menu/gif-list/gif-list';
+import { Gif } from 'src/interfaces/gif.interface';
 
 @Component({
   selector: 'app-search-page',
-  imports: [],
+  imports: [GifList],
   templateUrl: './search-page.html',
-  styles: ``,
+  styles: ``, 
 })
 export default class SearchPage {
 
+  gifService = inject(GifService)
+  gifs = signal<Gif[]>([]);  
+
+  onSearch(query: string) {
+    this.gifService.searchGifs(query ).subscribe((resp)=>{
+     this.gifs.set(resp); 
+      
+    })
+  }
 }
--- src/services/gif.service.ts
+++ src/services/gif.service.ts
@@ -4,9 +4,9 @@ import { environment } from '../environments/environment';
 import { GiphyResponse } from 'src/interfaces/giphy.interface';
 import { Gif } from 'src/interfaces/gif.interface';
 import { GifMapper } from 'src/app/gifs/components/gifs-side-menu/mapper/gif.mapper';
+import { map } from 'rxjs/operators';
 
 @Injectable({ providedIn: 'root' })
-
 export class GifService {
   private http = inject(HttpClient);
   trendingGifs = signal<Gif[]>([]);
@@ -16,23 +16,42 @@ export class GifService {
     this.loadTrendingGifs();
   }
 
-  
-
   loadTrendingGifs() {
     this.http
       .get<GiphyResponse>(`${environment.giphyUrl}/gifs/trending`, {
         params: {
-          api_key:environment.giphyApikey,
+          api_key: environment.giphyApikey,
           limit: '15',
         },
       })
       .subscribe((resp) => {
         const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
         this.trendingGifs.set(gifs);
-        this.trendingGifsLoafing.set(false)
-        console.log({gifs});
-        
-        
+        this.trendingGifsLoafing.set(false);
+        console.log({ gifs });
       });
+  }
+
+  //Search gifs
+  searchGifs(query: string) {
+    return this.http
+      .get<GiphyResponse>(`${environment.giphyUrl}/gifs/search`, {
+        params: {
+          api_key: environment.giphyApikey,
+          limit: '15',
+          q: query,
+        },
+      })
+      .pipe(
+        map((resp) => resp.data),
+        map((items) => GifMapper.mapGiphyItemsToGifArray(items)),
+      );
+
+    // .subscribe((resp) => {
+    //  const gifs = GifMapper.mapGiphyItemsToGifArray(resp.data);
+
+    //   console.log({search:gifs});
+
+    // });
   }
 }
