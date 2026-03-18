2715b8afc4903561482ff6626aeed83c026dceff
Robertorodriguezbqz <robertorodriguezbqz@gmail.com>
2026-03-18 19:22:12 -0300

    ultimo avance en macos

--- src/app/pages/trending-page/trending-page.ts
+++ src/app/pages/trending-page/trending-page.ts
@@ -1,15 +1,31 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, ElementRef, inject, signal, viewChild } from '@angular/core';
 import { GifList } from 'src/app/gifs/components/gifs-side-menu/gif-list/gif-list';
 import { GifListItems } from 'src/app/gifs/components/gifs-side-menu/gif-list/gif-list-items/gif-list-items';
 import { GifService } from 'src/services/gif.service';
 
 @Component({
   selector: 'app-trending-page',
-  imports: [GifList],
+  // imports: [GifList],
   templateUrl: './trending-page.html',
   styles: ``,
 })
 export default class TrendingPage {
   gifService = inject(GifService);
+  scrollDivRef = viewChild<ElementRef<HTMLDivElement>>('groupDiv');
+
+  onScroll(event: Event){
+    const scrollDiv = this.scrollDivRef()?.nativeElement;
+   if (!scrollDiv) return; 
+
+   const scrollTop = scrollDiv.scrollTop;
+   const clientHeight = scrollDiv.clientHeight;
+   const scrollHeight = scrollDiv.scrollHeight;
+    
+    const isAtBottom = scrollTop + clientHeight >= scrollHeight;
+    if (isAtBottom) {
+      this.gifService.loadTrendingGifs();      
+    }
+
+  }
   
 }
